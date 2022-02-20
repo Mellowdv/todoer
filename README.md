0794d9037fcd13b6788a8e6f74bf968191cd7a87
Mellowdv <tym.talik@gmail.com>
2022-02-20 16:25:22 +0100

    Added display menu, display lists, quit and add a list functionalities

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # todoer
-Command line to-do list app in C++.
+The goal for the todoer project is to create a simple app that will work in the command line.
+
+The app should display a menu that lets the user create a new list, open an already existing one or delete.
+The user should also be able to simply quit.
+
+The main "database" of lists should be saved in a .txt file.
+The lists themselves should also be saved in simple .txt format in the app's folder.
+Lists should be editable and provide task adding, marking as done and deleting functionality.
+Lists should have names.
+
+The user should be able to exit a list and go back to the menu.
+
+Implemented so far:
+    - Main menu
+    - Display the master list (of lists)
+    - Add new lists to the list
+    - Quit
+
+TO DO:
+    - Open a list
+    - Add/delete tasks
+    - Delete a list
+    - Save everything to file
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 bool check_empty(std::string file_name) // this expects a full file name, along with the extension
 {
@@ -19,26 +20,99 @@ bool check_empty(std::string file_name) // this expects a full file name, along 
     }
 }
 
+void display_menu()
+{
+    std::cout << "Welcome to Todoer";
+    std::cout << "\nThe available options are: "
+              << "\nL - display all the To-do lists."
+              << "\nO list number - opens a list (i.e. O 2, opens list no. 2)."
+              << "\nA - adds a new list and prompts for a name for it."
+              << "\nD list number - deletes a list"
+              << "\nQ - quits Todoer.\n";
+}
+
+std::vector<std::string> grab_lists(const std::string &file_name)
+{
+    std::ifstream input(file_name);
+    std::vector<std::string> lists_vector {};
+    std::string line {};
+    while(std::getline(input, line))
+        lists_vector.push_back(line);
+    input.close();
+    return lists_vector;
+}
+
+std::vector<std::string> add_position(std::vector<std::string> &list)
+{
+    int positions_added {};
+    std::string user_input {};
+    std::cin.ignore(1000, '\n');
+
+    std::cout << "\nEnter Q to discard changes and return to the menu."
+              << "\nEnter S to save all changes and return to the menu."
+              << "\nPlease enter a position (list or task name): ";
+    while(getline(std::cin, user_input))
+    {
+        if (user_input == "Q" || user_input == "q")
+        {
+            for (int i = 0; i < positions_added; i++)
+                list.pop_back();
+            return list;
+        }
+        else if (user_input == "S" || user_input == "s")
+        {
+            return list;
+        }
+        else
+        {
+            list.push_back(user_input);
+            positions_added++;
+            std::cout << "List added, please enter another name, S or Q: ";
+        }
+    }
+    return list;
+}
+
+void display_lists(const std::vector<std::string> &lists_vector)
+{
+    for (size_t i = 0; i < lists_vector.size(); ++i)
+        std::cout << i + 1 << ". " << lists_vector.at(i) << std::endl;
+}
+
 int main()
 {
-    std::ofstream list_index;
-    std::ofstream list;
-    std::string name;
     const std::string list_base = "list_index.txt";
-    list_index.open(list_base, std::ios_base::app);
-    if (!list_index)
-        return 1;
-    std::cout << "Enter a list name: ";
-    std::getline(std::cin, name);
-
-    if (!check_empty(list_base))
-       list_index << "\n" << name;
-    else
-       list_index << name;
-
-    list.open(name + ".txt", std::ios_base::app);
-    list.close();
-
-    list_index.close();
+    std::vector<std::string> lists {};
+    lists = grab_lists(list_base);
+    char selection {};
+    display_menu();
+    while (std::cin >> selection)
+    {
+        selection = toupper(selection);
+        switch (selection)
+        {
+            case 'A':
+            {
+                lists = add_position(lists);
+                break;
+            }
+            case 'L':
+            {
+                display_lists(lists);
+                break;
+            }
+            case 'Q':
+            {
+                std::cout << "\nThanks for using Todoer :)"
+                          << "\nBye!";
+                return 0;
+            }
+            default:
+            {
+                std::cout << "Command not recognized\n";
+                break;
+            }
+        }
+    }
     return 0;
 }
